3408d4311ef6eb023fcd39fa8ff1c230c5cf937e
xuxiaoshan <xuxiaoshan@51diandiangou.com>
2017-09-27 18:17:48 +0800

    init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
- selector: 'ali-home',
- templateUrl: 'app/home/home.component.html'
+    selector: 'ali-home',
+    templateUrl: 'app/home/home.component.html',
+    styleUrls: ['app/home/home.component.css']
 })
 
 export class HomeComponent implements OnInit {
- constructor() { }
+    constructor() {
+    }
 
- ngOnInit() { }
+    ngOnInit() {
+    }
 }
--- src/app/mine/mine.component.ts
+++ src/app/mine/mine.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
- selector: 'mine',
- templateUrl: 'app/mine/mine.component.html'
+    selector: 'mine',
+    templateUrl: 'app/mine/mine.component.html',
+    styleUrls: ['app/mine/mine.component.css']
 })
 
 export class MineComponent implements OnInit {
- constructor() { }
+    constructor() {
+    }
 
- ngOnInit() { }
+    ngOnInit() {
+    }
 }
